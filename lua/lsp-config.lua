1e7da7c8fe9b352e48f2bf142450bbb691c0d679
Jacopo Binosi <b4inoz@gmail.com>
2023-01-22 13:42:02 +0100

    module pattern implemented

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,65 +1,142 @@
-SERVERS = {
-  "bashls",
-  "dockerls",
-  "pyright",
-  --"sumneko_lua",
-  --"terraformls",
-  "yamlls",
-}
+local M = {}
 
-require 'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-  },
-  ensure_installed = {
-    "bash",
-    "dockerfile",
-    "python",
-    --"lua",
-    --"terraform",
-    "vim",
-    "yaml",
-  },
-}
+function M.setup()
+    local lsp_servers = {
+      "bashls",
+        "dockerls",
+      "pyright",
+      --"sumneko_lua",
+      --"terraformls",
+      "yamlls",
+    }
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false
-}
-)
+  -- highlitimg
+  require 'nvim-treesitter.configs'.setup {
+    highlight = {
+      enable = true,
+    },
+    ensure_installed = {
+      "bash",
+      "dockerfile",
+      "python",
+      "lua",
+      "terraform",
+      "vim",
+      "yaml",
+    },
+  }
 
-require('mason').setup({})
+  -- language servers manager
+  require('mason').setup({})
+  require('mason-lspconfig').setup({
+    ensure_installed = lsp_servers,
+    automatic_installation = false,
+  })
 
-require('mason-lspconfig').setup({
-  ensure_installed = SERVERS,
-  automatic_installation = false,
-})
+  -- language server providers configuration
+  -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  --
+  -- npm i -g bash-language-server
+  require 'lspconfig'.bashls.setup {}
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
---
--- npm i -g bash-language-server
-require 'lspconfig'.bashls.setup {}
+  -- npm install -g dockerfile-language-server-nodejs
+  require 'lspconfig'.dockerls.setup {}
 
--- npm install -g dockerfile-language-server-nodejs
-require 'lspconfig'.dockerls.setup {}
+  -- https://github.com/microsoft/pyright
+  require 'lspconfig'.pyright.setup {}
 
--- https://github.com/microsoft/pyright
-require 'lspconfig'.pyright.setup {}
-
--- https://github.com/sumneko/lua-language-server/wiki/Getting-Started#command-line
-require 'lspconfig'.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim', 'use' }
+  -- https://github.com/sumneko/lua-language-server/wiki/Getting-Started#command-line
+  require 'lspconfig'.sumneko_lua.setup {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim', 'use' }
+        }
       }
     }
   }
-}
 
--- https://github.com/hashicorp/terraform-ls/releases
-require 'lspconfig'.terraformls.setup {}
+  -- https://github.com/hashicorp/terraform-ls/releases
+  require 'lspconfig'.terraformls.setup {}
 
--- yarn global add yaml-language-server
-require 'lspconfig'.yamlls.setup {}
+  -- yarn global add yaml-language-server
+  require 'lspconfig'.yamlls.setup {}
 
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false
+  }
+  )
+
+  -- completion/docs
+  local cmp = require 'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  for _, server in pairs(lsp_servers) do
+    require('lspconfig')[server].setup {
+      capabilities = capabilities
+    }
+  end
+
+  -- autopairs
+  require 'nvim-autopairs'.setup {}
+  -- If you want insert `(` after select function or method item
+  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  cmp.event:on(
+    'confirm_done',
+    cmp_autopairs.on_confirm_done()
+  )
+end
+
+return M
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,82 +1,88 @@
-HOME = os.getenv("HOME")
+local M = {}
 
-vim.g.mapleader = ','
-vim.g.maplocalleader = '\\'
+function M.setup()
+  local home = os.getenv("HOME")
 
--- basic settings
-vim.opt.encoding = "utf-8"
-vim.opt.backspace = "indent,eol,start" -- backspace works on every char in insert mode
-vim.opt.completeopt = 'menu,menuone,noselect'
-vim.opt.history = 1000
-vim.opt.dictionary = '/usr/share/dict/words'
-vim.opt.startofline = true
-vim.opt.mouse = nil
+  vim.g.mapleader = ','
+  vim.g.maplocalleader = '\\'
 
--- Mapping waiting time
-vim.opt.timeout = false
-vim.opt.ttimeout = true
-vim.opt.ttimeoutlen = 100
+  -- basic settings
+  vim.opt.encoding = "utf-8"
+  vim.opt.backspace = "indent,eol,start" -- backspace works on every char in insert mode
+  vim.opt.completeopt = 'menu,menuone,noselect'
+  vim.opt.history = 1000
+  vim.opt.dictionary = '/usr/share/dict/words'
+  vim.opt.startofline = true
+  vim.opt.mouse = nil
 
--- Display
-vim.opt.showmatch  = true -- show matching brackets
-vim.opt.scrolloff = 3 -- always show 3 rows from edge of the screen
-vim.opt.synmaxcol = 300 -- stop syntax highlight after x lines for performance
-vim.opt.laststatus = 2 -- always show status line
+  -- Mapping waiting time
+  vim.opt.timeout = false
+  vim.opt.ttimeout = true
+  vim.opt.ttimeoutlen = 100
 
-vim.opt.list = false -- do not display white characters
-vim.opt.foldenable = false
-vim.opt.foldlevel = 4 -- limit folding to 4 levels
-vim.opt.foldmethod = 'syntax' -- use language syntax to generate folds
-vim.opt.wrap = false --do not wrap lines even if very long
-vim.opt.eol = false -- show if there's no eol char
-vim.opt.showbreak= '↪' -- character to show when line is broken
+  -- Display
+  vim.opt.showmatch  = true -- show matching brackets
+  vim.opt.scrolloff = 3 -- always show 3 rows from edge of the screen
+  vim.opt.synmaxcol = 300 -- stop syntax highlight after x lines for performance
+  vim.opt.laststatus = 2 -- always show status line
 
--- Sidebar
-vim.opt.number = true -- line number on the left
-vim.opt.modelines = 0
-vim.opt.showcmd = true -- display command in bottom bar
+  vim.opt.list = false -- do not display white characters
+  vim.opt.foldenable = false
+  vim.opt.foldlevel = 4 -- limit folding to 4 levels
+  vim.opt.foldmethod = 'syntax' -- use language syntax to generate folds
+  vim.opt.wrap = false --do not wrap lines even if very long
+  vim.opt.eol = false -- show if there's no eol char
+  vim.opt.showbreak= '↪' -- character to show when line is broken
 
--- Search
-vim.opt.incsearch = true -- starts searching as soon as typing, without enter needed
-vim.opt.ignorecase = true -- ignore letter case when searching
-vim.opt.smartcase = true -- case insentive unless capitals used in search
+  -- Sidebar
+  vim.opt.number = true -- line number on the left
+  vim.opt.modelines = 0
+  vim.opt.showcmd = true -- display command in bottom bar
 
-vim.opt.matchtime = 2 -- delay before showing matching paren
-vim.opt.mps = vim.o.mps .. ",<:>"
+  -- Search
+  vim.opt.incsearch = true -- starts searching as soon as typing, without enter needed
+  vim.opt.ignorecase = true -- ignore letter case when searching
+  vim.opt.smartcase = true -- case insentive unless capitals used in search
 
--- White characters
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.tabstop = 2 -- 1 tab = 2 spaces
-vim.opt.shiftwidth = 2 -- indentation rule
-vim.opt.formatoptions = 'qnj1' -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
-vim.opt.expandtab = true -- expand tab to spaces
+  vim.opt.matchtime = 2 -- delay before showing matching paren
+  vim.opt.mps = vim.o.mps .. ",<:>"
 
--- Backup files
-vim.opt.backup = true -- use backup files
-vim.opt.writebackup = false
-vim.opt.swapfile = false -- do not use swap file
-vim.opt.undodir = HOME .. '/.config/nvim/tmp/undo//'     -- undo files
-vim.opt.backupdir = HOME .. '/.config/nvim/tmp/backup//' -- backups
-vim.opt.directory = '/.config/nvim/tmp/swap//'   -- swap files
+  -- White characters
+  vim.opt.autoindent = true
+  vim.opt.smartindent = true
+  vim.opt.tabstop = 2 -- 1 tab = 2 spaces
+  vim.opt.shiftwidth = 2 -- indentation rule
+  vim.opt.formatoptions = 'qnj1' -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
+  vim.opt.expandtab = true -- expand tab to spaces
 
-vim.cmd([[
-  au FileType python                  set ts=4 sw=4
-  au BufRead,BufNewFile *.md          set ft=mkd tw=80 syntax=markdown
-]])
+  -- Backup files
+  vim.opt.backup = true -- use backup files
+  vim.opt.writebackup = false
+  vim.opt.swapfile = false -- do not use swap file
+  vim.opt.undodir = home .. '/.config/nvim/tmp/undo//'     -- undo files
+  vim.opt.backupdir = home .. '/.config/nvim/tmp/backup//' -- backups
+  vim.opt.directory = '/.config/nvim/tmp/swap//'   -- swap files
 
--- Commands mode
-vim.opt.wildmenu = true -- on TAB, complete options for system command
-vim.opt.wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc'
+  vim.cmd([[
+    au FileType python                  set ts=4 sw=4
+    au BufRead,BufNewFile *.md          set ft=mkd tw=80 syntax=markdown
+  ]])
 
--- Only show cursorline in the current window and in normal mode.
-vim.cmd([[
-  augroup cline
-      au!
-      au WinLeave * set nocursorline
-      au WinEnter * set cursorline
-      au InsertEnter * set nocursorline
-      au InsertLeave * set cursorline
-  augroup END
-]])
+  -- Commands mode
+  vim.opt.wildmenu = true -- on TAB, complete options for system command
+  vim.opt.wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc'
 
+  -- Only show cursorline in the current window and in normal mode.
+  vim.cmd([[
+    augroup cline
+        au!
+        au WinLeave * set nocursorline
+        au WinEnter * set cursorline
+        au InsertEnter * set nocursorline
+        au InsertLeave * set cursorline
+    augroup END
+  ]])
+
+end
+
+return M
