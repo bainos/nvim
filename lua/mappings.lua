be083cc2e80c49d4f3d062ea84f16de2f3eb7205
Jacopo Binosi <b4inoz@gmail.com>
2023-01-22 13:45:37 +0100

    module pattern implemented (#1)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,21 +1,39 @@
-vim.keymap.set('n', '<C-b>', ':noh<cr>:call clearmatches()<cr>', {})
-vim.keymap.set('n', '<Leader>w', ':w<cr>', {})
-vim.keymap.set('n', '<Leader>qa', ':qa<cr>', {})
+local M = {}
 
-local lsp_diagnostic = vim.diagnostic
-vim.keymap.set('n', '<Leader>d', lsp_diagnostic.open_float, {})
-vim.keymap.set('n', '<Leader>n', lsp_diagnostic.goto_next, {})
-vim.keymap.set('n', '<Leader>p', lsp_diagnostic.goto_prev, {})
+function M.setup()
+  -- search
+  vim.keymap.set('n', '<C-b>', ':noh<cr>:call clearmatches()<cr>', {})
+  vim.keymap.set('n', '*', '*N', {})
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  -- buffers
+  vim.keymap.set('n', '<Leader>w', ':w<cr>', {})
+  vim.keymap.set('n', '<Leader>qa', ':qa<cr>', {})
+  vim.keymap.set('n', '<Leader>#', ':b#<cr>', {})
 
-local api = vim.api
-api.nvim_create_autocmd(
-  "BufWritePre",
-  --{ command = 'lua vim.lsp.buf.format()' }
-  { callback = vim.lsp.buf.format }
-)
+  -- diagnostic
+  local lsp_diagnostic = vim.diagnostic
+  vim.keymap.set('n', '<Leader>d', lsp_diagnostic.open_float, {})
+  vim.keymap.set('n', '<Leader>n', lsp_diagnostic.goto_next, {})
+  vim.keymap.set('n', '<Leader>p', lsp_diagnostic.goto_prev, {})
+
+  -- file/buffer manager
+  local builtin = require('telescope.builtin')
+  vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+  local function buf_format()
+    vim.lsp.buf.format()
+  end
+
+  local api = vim.api
+  api.nvim_create_autocmd(
+    { "BufWritePre", },
+    --{ command = 'lua vim.lsp.buf.format()' }
+    { callback = buf_format }
+  --{ callback = vim.lsp.buf.format }
+  )
+end
+
+return M
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,48 +1,59 @@
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+local M = {}
 
-  -- common
-  use 'andymass/vim-matchup' -- matching parens and more
-  use 'bronson/vim-trailing-whitespace' -- highlight trailing spaces
-  use "nvim-lua/plenary.nvim"
+function M.setup()
+  local packer_conf = {
+    profile = {
+      enable = true,
+      threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+    },
+  }
 
-  -- general dev
-  use 'neovim/nvim-lspconfig'
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
-  --use({
-  --"glepnir/lspsaga.nvim",
-  --branch = "main",
-  --config = function()
-  --require('lspsaga').setup({
-  --error_sign = '!',
-  --warn_sign = '^',
-  --hint_sign = '?',
-  --infor_sign = '~',
-  --border_style = "round",
-  --code_action_prompt = {
-  --enable = false
-  --}
-  --})
-  --end,
-  --})
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'scrooloose/nerdcommenter' -- commenting shortcuts
-  use "windwp/nvim-autopairs"
+  local function plugins(use)
+    use 'wbthomason/packer.nvim'
 
-  -- search
-  use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
+    -- common
+    use 'andymass/vim-matchup' -- matching parens and more
+    use 'bronson/vim-trailing-whitespace' -- highlight trailing spaces
+    use "nvim-lua/plenary.nvim"
+		--use "lewis6991/impatient.nvim"
 
-  -- python
-  use { 'Vimjas/vim-python-pep8-indent', ft = 'python' }
+    -- general dev
+    use 'neovim/nvim-lspconfig'
+    use "williamboman/mason.nvim"
+    use "williamboman/mason-lspconfig.nvim"
+    -- -- completion
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/vim-vsnip'
+    -- -- highlighting
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    -- -- comments - <leader>c<space>
+    use 'scrooloose/nerdcommenter' -- commenting shortcuts
+    -- -- parens and quotes
+    use "windwp/nvim-autopairs"
 
-  -- themes
-  use "ellisonleao/gruvbox.nvim"
-end)
+    -- search - buffer/file browser
+    use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
+
+    -- python
+    use { 'Vimjas/vim-python-pep8-indent', ft = 'python' }
+
+    -- themes
+    use "ellisonleao/gruvbox.nvim"
+  end
+
+  local packer = require("packer")
+
+  -- Performance
+  --pcall(require, "impatient")
+  -- pcall(require, "packer_compiled")
+
+  packer.init(packer_conf)
+  packer.startup(plugins)
+end
+
+return M
