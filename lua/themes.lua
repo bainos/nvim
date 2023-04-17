f65956784fb810e51fed9b1eef0f507ad39c6a43
jacopo binosi <jacopo.binosi@musixmatch.com>
2023-04-17 15:03:18 +0000

    fix gruvebox deprecation on italic

--- lua/themes.lua
+++ lua/themes.lua
@@ -7,7 +7,7 @@ function M.setup()
         undercurl = true,
         underline = true,
         bold = true,
-        italic = true,
+        --italic = true,
         strikethrough = true,
         invert_selection = false,
         invert_signs = false,
