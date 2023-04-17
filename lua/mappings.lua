867a97b3e5395a1b6b7dcd38a28b0530f8c451a2
jacopo binosi <jacopo.binosi@musixmatch.com>
2023-04-17 15:05:10 +0000

    leader to Leader

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -28,10 +28,10 @@ function M.setup()
 
     -- file/buffer manager
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
 
     local function buf_format()
         vim.lsp.buf.format()
