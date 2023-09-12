a5f3735c7806ba566bc6728f2f9190dab707f730
jacopo binosi <jacopo.binosi@musixmatch.com>
2023-09-12 14:48:28 +0000

    workaround C-style comments for shell script + lint

--- lua/settings.lua
+++ lua/settings.lua
@@ -19,39 +19,46 @@ function M.setup()
     vim.opt.history = 1000
     if is_linux then vim.opt.dictionary = '/usr/share/dict/words' end
     vim.opt.startofline = true
-    vim.opt.mouse = nil
+    vim.opt.mouse       = nil
+    -- fix '#' comment indentation
+    -- https://unix.stackexchange.com/questions/106526/stop-vim-from-messing-up-my-indentation-on-comments
+    -- http://neovim.io/doc/user/indent.html
+    vim.opt.smartindent = false
+    vim.opt.cindent     = true
+    vim.opt.cinkeys:remove '0#'
+    vim.opt.indentkeys:remove '0#'
 
     -- Mapping waiting time
-    vim.opt.timeout = false
-    vim.opt.ttimeout = true
+    vim.opt.timeout     = false
+    vim.opt.ttimeout    = true
     vim.opt.ttimeoutlen = 100
 
     -- Display
-    vim.opt.showmatch  = true -- show matching brackets
-    vim.opt.scrolloff  = 3 -- always show 3 rows from edge of the screen
-    vim.opt.synmaxcol  = 300 -- stop syntax highlight after x lines for performance
-    vim.opt.laststatus = 2 -- always show status line
+    vim.opt.showmatch   = true -- show matching brackets
+    vim.opt.scrolloff   = 3 -- always show 3 rows from edge of the screen
+    vim.opt.synmaxcol   = 300 -- stop syntax highlight after x lines for performance
+    vim.opt.laststatus  = 2 -- always show status line
 
-    vim.opt.list = false -- do not display white characters
-    vim.opt.foldenable = false
-    vim.opt.foldlevel = 4 -- limit folding to 4 levels
-    vim.opt.foldmethod = 'syntax' -- use language syntax to generate folds
-    vim.opt.wrap = false --do not wrap lines even if very long
-    vim.opt.eol = false -- show if there's no eol char
-    vim.opt.showbreak = '↪' -- character to show when line is broken
+    vim.opt.list        = false -- do not display white characters
+    vim.opt.foldenable  = false
+    vim.opt.foldlevel   = 4 -- limit folding to 4 levels
+    vim.opt.foldmethod  = 'syntax' -- use language syntax to generate folds
+    vim.opt.wrap        = false --do not wrap lines even if very long
+    vim.opt.eol         = false -- show if there's no eol char
+    vim.opt.showbreak   = '↪' -- character to show when line is broken
 
     -- Sidebar
-    vim.opt.number = false -- line number on the left
-    vim.opt.modelines = 0
-    vim.opt.showcmd = true -- display command in bottom bar
+    vim.opt.number      = false -- line number on the left
+    vim.opt.modelines   = 0
+    vim.opt.showcmd     = true  -- display command in bottom bar
 
     -- Search
-    vim.opt.incsearch = true -- starts searching as soon as typing, without enter needed
-    vim.opt.ignorecase = true -- ignore letter case when searching
-    vim.opt.smartcase = true -- case insentive unless capitals used in search
+    vim.opt.incsearch   = true -- starts searching as soon as typing, without enter needed
+    vim.opt.ignorecase  = true -- ignore letter case when searching
+    vim.opt.smartcase   = true -- case insentive unless capitals used in search
 
-    vim.opt.matchtime = 2 -- delay before showing matching paren
-    vim.opt.mps = vim.o.mps .. ',<:>'
+    vim.opt.matchtime   = 2    -- delay before showing matching paren
+    vim.opt.mps         = vim.o.mps .. ',<:>'
 
     -- White characters
     --vim.opt.autoindent = true
@@ -62,12 +69,12 @@ function M.setup()
     --vim.opt.expandtab = true -- expand tab to spaces
 
     -- Backup files
-    vim.opt.backup = true -- use backup files
+    vim.opt.backup      = true                                 -- use backup files
     vim.opt.writebackup = false
-    vim.opt.swapfile = false -- do not use swap file
-    vim.opt.undodir = home .. '/.config/nvim/tmp/undo//' -- undo files
-    vim.opt.backupdir = home .. '/.config/nvim/tmp/backup//' -- backups
-    vim.opt.directory = '/.config/nvim/tmp/swap//' -- swap files
+    vim.opt.swapfile    = false                                -- do not use swap file
+    vim.opt.undodir     = home .. '/.config/nvim/tmp/undo//'   -- undo files
+    vim.opt.backupdir   = home .. '/.config/nvim/tmp/backup//' -- backups
+    vim.opt.directory   = '/.config/nvim/tmp/swap//'           -- swap files
 
     --vim.cmd([[
     --au FileType python                  set ts=4 sw=4
@@ -75,8 +82,9 @@ function M.setup()
     --]])
 
     -- Commands mode
-    vim.opt.wildmenu = true -- on TAB, complete options for system command
-    vim.opt.wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc'
+    vim.opt.wildmenu    = true -- on TAB, complete options for system command
+    vim.opt.wildignore  =
+    'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc'
 
     -- Only show cursorline in the current window and in normal mode.
     vim.cmd [[
