0db2d3737d147c9a9a4eb9e5a8fac088b674b359
jacopo binosi <jacopo.binosi@musixmatch.com>
2023-10-02 13:17:27 +0000

    fix custom filetypes

--- lua/config/custom_filetypes.lua
+++ lua/config/custom_filetypes.lua
@@ -9,6 +9,7 @@ function M.setup()
             pattern = { '*.yml', },
             callback = function()
                 api.nvim_buf_set_option(0, 'filetype', 'azp')
+                vim.cmd ':set syn=yaml'
             end,
         }
     )
@@ -18,6 +19,7 @@ function M.setup()
             pattern = { '*/manifests/*.yaml', '*/resources/*.yaml', },
             callback = function()
                 api.nvim_buf_set_option(0, 'filetype', 'k8s')
+                vim.cmd ':set syn=yaml'
             end,
         }
     )
@@ -27,6 +29,7 @@ function M.setup()
             pattern = { '*/templates/*.yaml', '*/templates/*.tpl', 'values.yaml', 'values*.yaml', },
             callback = function()
                 api.nvim_buf_set_option(0, 'filetype', 'helm')
+                vim.cmd ':set syn=yaml'
             end,
         }
     )
