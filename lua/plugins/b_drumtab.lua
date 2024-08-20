5e7b10b98c8519ef1e2c3009ed820c056f0ca3b2
Jacopo Binosi aka bainos <b4inoz@gmail.com>
2024-08-20 12:35:02 +0200

    ful refactoring

--- lua/plugins/b_drumtab.lua
+++ lua/plugins/b_drumtab.lua
@@ -7,6 +7,9 @@ local drum_parts = {
     'BD',
 }
 
+-- ----------------
+-- DRAW DRUMTABS
+-- ----------------
 -- Function to draw the drum tablature with dynamic measures and beats
 local function draw_drum_tab(args)
     -- Parse the arguments: measures, beats per measure, note value, subdivisions
@@ -46,33 +49,9 @@ local function draw_drum_tab(args)
     vim.api.nvim_win_set_cursor(0, { cursor[1] - 4, cursor[2] + 4, })
 end
 
-local function parse_measure_count()
-    local buf_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-    local total_pipes = 0
-    local tab_lines = 0
-
-    for _, line in ipairs(buf_lines) do
-        -- Count the number of '|' characters in the line
-        local pipe_count = select(2, line:gsub('|', '|'))
-        total_pipes = total_pipes + pipe_count
-
-        -- Count the line if it has at least 2 pipes
-        if pipe_count >= 2 then
-            tab_lines = tab_lines + 1
-        end
-    end
-
-    -- Calculate total measures
-    if tab_lines > 0 then
-        local pipes = total_pipes - tab_lines
-        local total_measures = pipes / #drum_parts
-        return total_measures
-    else
-        return 0
-    end
-end
-
-
+-- ----------------
+-- PARSE DRUMTABS
+-- ----------------
 local function add_to_group(part, current_group)
     if not current_group:match '^<.*>$' then
         return '<' .. current_group .. ' ' .. part .. '>'
@@ -82,82 +61,139 @@ local function add_to_group(part, current_group)
     end
 end
 
-local function parse_drum_table(drum_table)
+local function parse_drum_linline(line, time_sig, subdivision)
     local result = {}
     local current_unit = ''
     local current_length = 1
+    local beats, beat_unit = time_sig:match '(%d+)/(%d+)'
+    beats = tonumber(beats)
+    beat_unit = tonumber(beat_unit)
+    local dash_length = beat_unit * subdivision
 
-    for i = 1, #drum_table do
-        if drum_table[i] == '-' then
-            if i == 1 then
-                current_unit = 'RRr'
-            else
-                current_length = current_length + 1
-            end
-        else
-            if current_unit ~= '' then
-                table.insert(result, current_unit .. current_length)
-            end
-            current_unit = drum_table[i]
-            current_length = 1
+    vim.notify('line: ' .. vim.inspect(line))
+    for i = 1, #line do
+        if line[i] == '-' and line[i - 1] == '|' then
+            line[i] = 'RRr'
         end
+
+        if line[i] == '|' or line[i] ~= '-' then
+            if current_unit ~= '' then
+                local note_length = dash_length / current_length
+                vim.notify('note_length: ' .. tostring(note_length))
+                table.insert(result, current_unit .. note_length)
+            end
+
+            current_length = 1
+            current_unit = (line[i] == '|' and '') or line[i]
+        else
+            current_length = current_length + 1
+        end
+
+        vim.notify(line[i] .. ' ' .. tostring(current_length))
     end
 
-    if current_unit ~= '' then
-        table.insert(result, current_unit .. current_length)
-    end
+    -- for i = 1, #line do
+    --     if i % beat_unit == 1 and line[i] == '-' then
+    --         line[i] = 'RRr'
+    --     end
+    --     if line[i] == '-' then
+    --         current_length = current_length + 1
+    --     else
+    --         if current_unit ~= '' then
+    --             local note_length = dash_length / current_length
+    --             table.insert(result, current_unit .. note_length)
+    --         end
+    --         current_unit = line[i]
+    --         current_length = 1
+    --     end
+    -- end
+    --
+    -- if current_unit ~= '' then
+    --     local note_length = dash_length / current_length
+    --     table.insert(result, current_unit .. note_length)
+    -- end
 
     return result
 end
 
-local function lilypondify(tab1)
-    local result = {}
+local function tab_subdivision(tab_line)
+    local _, first_pipe_end = tab_line:find '|'
+    local second_pipe_start = tab_line:find('|', first_pipe_end + 1)
+    return second_pipe_start - first_pipe_end - 1
+end
 
-    for part_name, part_content in pairs(tab1) do
-        for j = 1, #part_content do
-            local char = part_content:sub(j, j)
+local function collapse_rows(tab)
+    local time_sig = tab[1]:match '^TS (%d+/%d+)'
+    if time_sig then
+        table.remove(tab, 1)
+    else
+        time_sig = '4/4'
+    end
+
+    local subdivision = tab_subdivision(tab[1])
+
+    local result = {}
+    for _, line in ipairs(tab) do
+        local part_name = line:sub(1, 2)
+        local part_content = line:sub(4)
+        local j = 1
+        for i = 1, #part_content do
+            local char = part_content:sub(i, i)
 
             if char == '|' then
                 result[j] = char
+                j = j + 1
             elseif char ~= '-' then
                 if result[j] == nil or result[j] == '-' then
                     result[j] = part_name .. char
+                    j = j + 1
                 else
                     result[j] = add_to_group(part_name .. char, result[j])
+                    j = j + 1
                 end
             else
                 if result[j] == nil then
                     result[j] = char
                 end
+                j = j + 1
             end
         end
     end
 
-    local drum_table = {}
-    local line_tmp = {}
-    for i = 1, #result do
-        if result[i] == '|' then
-            table.insert(drum_table,
-                table.concat(
-                    parse_drum_table(line_tmp), ' '))
-            line_tmp = {}
-        else
-            table.insert(line_tmp, result[i])
+    return parse_drum_linline(result, time_sig, subdivision)
+end
+
+local function merge_tabs_within_group(group)
+    local merged_tabs = {}
+
+    for _, tab in ipairs(group) do
+        tab = collapse_rows(tab)
+        vim.notify('collapsed tab: ' .. vim.inspect(tab))
+
+        for _, v in ipairs(tab) do
+            table.insert(merged_tabs, v)
         end
     end
-    -- return merged_tab
-    return drum_table
+
+    return merged_tabs
 end
 
-local function get_buffer_lines(start_line, end_line)
-    return vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
+local function print_merged_tabs_and_groups(groups)
+    for _, group in ipairs(groups) do
+        local merged_tabs = merge_tabs_within_group(group)
+
+        for i = 1, #merged_tabs do
+            vim.api.nvim_buf_set_lines(0, -1, -1, false, { merged_tabs[i], })
+        end
+        vim.api.nvim_buf_set_lines(0, -1, -1, false, { '', }) -- Insert an empty line after each merged tab group
+
+        -- Add separator if there are more groups
+        if _ < #groups then
+            vim.api.nvim_buf_set_lines(0, -1, -1, false, { '---', '', })
+        end
+    end
 end
 
--- local function set_buffer_lines(start_line, new_lines)
---     vim.api.nvim_buf_set_lines(0, start_line, start_line, false, new_lines)
--- end
---
--- EXPERIMENTAL
 local function identify_tabs(buffer_lines)
     local groups = {}
     local current_group = {}
@@ -195,58 +231,11 @@ local function identify_tabs(buffer_lines)
     return groups
 end
 
-local function merge_tabs_within_group(group)
-    local merged_tabs = {}
-
-    for _, tab in ipairs(group) do
-        for _, line in ipairs(tab) do
-            local part_name = line:sub(1, 2)
-            local part_content = line:sub(4)
-
-            if not merged_tabs[part_name] then
-                merged_tabs[part_name] = part_content
-            else
-                merged_tabs[part_name] = merged_tabs[part_name] .. part_content:gsub('^|', '')
-            end
-        end
-    end
-
-    return lilypondify(merged_tabs)
-end
-
-local function print_merged_tabs_and_groups(groups)
-    for _, group in ipairs(groups) do
-        local merged_tabs = merge_tabs_within_group(group)
-
-        for i = 1, #merged_tabs do
-            vim.api.nvim_buf_set_lines(0, -1, -1, false, { merged_tabs[i], })
-        end
-        vim.api.nvim_buf_set_lines(0, -1, -1, false, { '', }) -- Insert an empty line after each merged tab group
-
-        -- Add separator if there are more groups
-        if _ < #groups then
-            vim.api.nvim_buf_set_lines(0, -1, -1, false, { '---', '', })
-        end
-    end
-end
-
--- EXPERIMENTAL END
-
-local function show_measure_count()
-    local measures = parse_measure_count()
-    local lines = get_buffer_lines(0, -1)
-
-    -- local tab1 = { unpack(lines, 1, 4), }
-    -- local tab2 = { unpack(lines, 6, 9), }
-    -- local merged_tab = merge_drum_tabs(tab1, tab2)
+local function lilypondify()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
     vim.cmd 'new'
 
-    -- Write the number of measures into the buffer
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, { 'Number of measures: ' .. measures, })
-    -- vim.api.nvim_buf_set_lines(0, 0, -1, false, { 'result: ' .. table.concat(merged_tab, ' '), })
-    -- vim.api.nvim_buf_set_lines(0, 0, -1, false, merged_tab)
-    -- set_buffer_lines(#lines, merged_tab)
     local groups = identify_tabs(lines)
     print_merged_tabs_and_groups(groups)
 end
@@ -261,11 +250,9 @@ function M.setup()
     vim.opt.breakindentopt = 'shift:4,sbr'
     vim.opt.breakat = '|'
 
-    -- Create a command to run the function with arguments
-    vim.api.nvim_create_user_command('DrawDrumTab', draw_drum_tab, { nargs = 1, })
+    vim.api.nvim_create_user_command('DrumTabDraw', draw_drum_tab, { nargs = 1, })
 
-    -- Create the command to show the measure count
-    vim.api.nvim_create_user_command('ShowMeasureCount', show_measure_count, {})
+    vim.api.nvim_create_user_command('DrumTabLilypondify', lilypondify, {})
 end
 
 return M
